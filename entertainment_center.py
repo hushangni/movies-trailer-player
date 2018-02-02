b2c83f59174418e20ede4a646d4da5cd7ef86fc3
hushangni <shangnihu@gmail.com>
2018-02-01 23:12:30 -0500

    Added big hero 6

--- entertainment_center.py
+++ entertainment_center.py
@@ -5,7 +5,7 @@ import media
 disaster_artist = media.Movie("Disaster Artist",
                               "Making of the film The Room",
                               "https://upload.wikimedia.org/wikipedia/en/c/c9/TheDisastorArtistTeaserPoster.jpg",
-                              "https://www.youtube.com/watch?v=sPSJYXi7BWA");
+                              "https://www.youtube.com/watch?v=sPSJYXi7BWA")
 
 toy_story = media.Movie("Toy Story",
                         "A story of a boy and his toys that come alive!",
@@ -21,6 +21,12 @@ red_sparrow = media.Movie("Red Sparrow",
                           "Russian spy exposing an American mole in Russia",
                           "https://upload.wikimedia.org/wikipedia/en/5/5a/Red_Sparrow.png",
                           "https://www.youtube.com/watch?v=PmUL6wMpMWw")
+
+big_hero_6 = media.Movie("Big Hero 6",
+                          "In San Fransokyo a boy and a robot make friends",
+                          "https://vignette.wikia.nocookie.net/filmguide/images/b/bb/Big-hero-6-poster-baymax-hi-res.jpg/revision/latest?cb=20150222112242",
+                          "https://www.youtube.com/watch?v=z3biFxZIJOQ"
+                          )
 
 black_panther = media.Movie("Black Panther",
                             "Son of the King of Wakanda returns home to claim the throne",
@@ -42,6 +48,8 @@ shawshank = media.Movie("Shawshank Redemption",
                         "https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg",
                         "https://www.youtube.com/watch?v=6hB3S9bIaco")
 
+
+
 zootopia = media.Movie("Zootopia",
                        "A city of animals, a fox and bunny work together to solve a crime.",
                        "https://upload.wikimedia.org/wikipedia/en/e/ea/Zootopia.jpg",
@@ -54,7 +62,7 @@ wonder_woman = media.Movie("Wonder Woman",
 
 
 
-movies = [disaster_artist, toy_story, get_out, red_sparrow, black_panther, school_of_rock, moana, shawshank,
+movies = [disaster_artist, toy_story, get_out, red_sparrow, big_hero_6, black_panther, school_of_rock, moana, shawshank,
           zootopia, wonder_woman]
 fresh_tomatoes.open_movies_page(movies)
 
